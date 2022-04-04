a62ffeb867103015f4e030d6b71e47d01f7b2325
kwakjaeyoon <jaeyoon5636@gmail.com>
2022-04-04 13:59:26 +0900

    Fix mongodb.js environment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const AutoLoad = require('fastify-autoload')
+require('dotenv').config()
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -3,11 +3,13 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function (fastify, opts) {
-  if (process.env.NODE_ENV === 'test') {
-    return
-  }
+  // if (process.env.NODE_ENV === 'baedal') {
+  //   return
+  // }
   fastify.register(require('fastify-mongodb'), {
     forceClose: true,
-    url: 'mongodb://root:example@teami-loadbalancer-cb78ef30e32ffd53.elb.ap-northeast-2.amazonaws.com:27017/'
+    url: process.env.MONGODB_ENDPOINT
+    
   })
+  console.log("dbok");
 })
